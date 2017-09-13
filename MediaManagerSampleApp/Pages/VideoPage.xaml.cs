b111db6653d759ba89305bc0cd2c80e13d5ce49d
JuneCho23 <junecho.ucb@gmail.com>
2017-09-12 17:26:24 -0700

    added CB C#

--- MediaManagerSampleApp/Pages/VideoPage.xaml.cs
+++ MediaManagerSampleApp/Pages/VideoPage.xaml.cs
@@ -7,9 +7,44 @@ namespace MediaManagerSampleApp
 {
     public partial class VideoPage : ContentPage
     {
-        public VideoPage()
-        {
-            InitializeComponent();
-        }
+private IPlaybackController PlaybackController => CrossMediaManager.Current.PlaybackController;
+
+		public VideosListLayout()
+		{
+			InitializeComponent();
+
+			//CrossMediaManager.Current.PlayingChanged += (sender, e) =>
+			//{
+			//  Device.BeginInvokeOnMainThread(() =>
+			//  {
+			//      //ProgressBar.Progress = e.Progress;
+			//      //Duration.Text = "" + e.Duration.TotalSeconds + " seconds";
+			//  });
+			//};
+
+		}
+
+		protected override void OnAppearing()
+		{
+
+			//PlaybackController.Play();
+			videoView.Source = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+			PlaybackController.Play();
+		}
+
+		void PlayClicked(object sender, System.EventArgs e)
+		{
+			PlaybackController.Play();
+		}
+
+		void PauseClicked(object sender, System.EventArgs e)
+		{
+			PlaybackController.Pause();
+		}
+
+		void StopClicked(object sender, System.EventArgs e)
+		{
+			PlaybackController.Stop();
+		}
     }
 }
