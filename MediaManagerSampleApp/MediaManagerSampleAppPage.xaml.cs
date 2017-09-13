5962c3c4af7868b48674a501d085813b975c7f1a
JuneCho23 <junecho.ucb@gmail.com>
2017-09-12 17:20:23 -0700

    Start

---
+++ MediaManagerSampleApp/MediaManagerSampleAppPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MediaManagerSampleApp
+{
+    public partial class MediaManagerSampleAppPage : ContentPage
+    {
+        public MediaManagerSampleAppPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
