fe0da8a746ba38552bf0e848f56d79eeb53c00f7
JuneCho23 <junecho.ucb@gmail.com>
2017-09-12 17:30:54 -0700

    CleanUp

--- MediaManagerSampleApp/Pages/VideoPage.xaml.cs
+++ MediaManagerSampleApp/Pages/VideoPage.xaml.cs
@@ -1,50 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Plugin.MediaManager;
+using Plugin.MediaManager.Abstractions;
+using Plugin.MediaManager.Forms;
 using Xamarin.Forms;
 
 namespace MediaManagerSampleApp
 {
     public partial class VideoPage : ContentPage
     {
-private IPlaybackController PlaybackController => CrossMediaManager.Current.PlaybackController;
+        private IPlaybackController PlaybackController => CrossMediaManager.Current.PlaybackController;
 
-		public VideosListLayout()
-		{
-			InitializeComponent();
+        public VideosListLayout()
+        {
+            InitializeComponent();
 
-			//CrossMediaManager.Current.PlayingChanged += (sender, e) =>
-			//{
-			//  Device.BeginInvokeOnMainThread(() =>
-			//  {
-			//      //ProgressBar.Progress = e.Progress;
-			//      //Duration.Text = "" + e.Duration.TotalSeconds + " seconds";
-			//  });
-			//};
+            //CrossMediaManager.Current.PlayingChanged += (sender, e) =>
+            //{
+            //  Device.BeginInvokeOnMainThread(() =>
+            //  {
+            //      //ProgressBar.Progress = e.Progress;
+            //      //Duration.Text = "" + e.Duration.TotalSeconds + " seconds";
+            //  });
+            //};
 
-		}
+        }
 
-		protected override void OnAppearing()
-		{
+        protected override void OnAppearing()
+        {
 
-			//PlaybackController.Play();
-			videoView.Source = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
-			PlaybackController.Play();
-		}
+            //PlaybackController.Play();
+            videoView.Source = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4";
+            PlaybackController.Play();
+        }
 
-		void PlayClicked(object sender, System.EventArgs e)
-		{
-			PlaybackController.Play();
-		}
+        void PlayClicked(object sender, System.EventArgs e)
+        {
+            PlaybackController.Play();
+        }
 
-		void PauseClicked(object sender, System.EventArgs e)
-		{
-			PlaybackController.Pause();
-		}
+        void PauseClicked(object sender, System.EventArgs e)
+        {
+            PlaybackController.Pause();
+        }
 
-		void StopClicked(object sender, System.EventArgs e)
-		{
-			PlaybackController.Stop();
-		}
+        void StopClicked(object sender, System.EventArgs e)
+        {
+            PlaybackController.Stop();
+        }
     }
 }
