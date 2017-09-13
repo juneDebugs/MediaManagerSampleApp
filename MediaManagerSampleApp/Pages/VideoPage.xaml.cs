d2214e9dfb61f9213f254debb86c7ffd96eac7bb
JuneCho23 <junecho.ucb@gmail.com>
2017-09-12 17:51:03 -0700

    ProgressBar

--- MediaManagerSampleApp/Pages/VideoPage.xaml.cs
+++ MediaManagerSampleApp/Pages/VideoPage.xaml.cs
@@ -15,14 +15,14 @@ namespace MediaManagerSampleApp
         {
             InitializeComponent();
 
-            //CrossMediaManager.Current.PlayingChanged += (sender, e) =>
-            //{
-            //    Device.BeginInvokeOnMainThread(() =>
-            //    {
-            //        ProgressBar.Progress = e.Progress;
-            //        Duration.Text = "" + e.Duration.TotalSeconds + " seconds";
-            //    });
-            //};
+            CrossMediaManager.Current.PlayingChanged += (sender, e) =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    ProgressBar.Progress = e.Progress;
+                    Duration.Text = "" + e.Duration.TotalSeconds + " seconds";
+                });
+            };
         }
 
         protected override void OnAppearing()
